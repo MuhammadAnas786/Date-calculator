fde1d275a4de6ea38a3a553c5bd41e4d02a315e9
MUHAMMAD ANAS <47847198+MuhammadAnas786@users.noreply.github.com>
2019-03-16 20:12:29 +0500

    Add files via upload

---
+++ driver.java
@@ -0,0 +1,21 @@
+/*-------M.Anas------*/
+
+public class driver{
+public static void main(String args[]){
+	
+	Date D=new Date(29,2,2005);
+	Date D2=new Date(29,2,2004);
+	Date D3=new Date(775,100,1999);
+	
+	System.out.println("Input (29,2,2005) ");
+	D.DisplayDate();
+	System.out.println("Input (29,2,2004) \nLeap Year:");
+	D2.DisplayDate();	
+	System.out.println("Input (775,100,1999)");
+	D3.DisplayDate();
+
+
+
+
+}
+}
